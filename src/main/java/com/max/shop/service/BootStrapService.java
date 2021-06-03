65772785dbbae52b5103b5881b3cb4c3a4b7743f
DUmetski <dmitry.umetsky@decentage.com>
2021-06-03 22:14:19 +0300

    add profile to BootStrapService

--- src/main/java/com/max/shop/service/BootStrapService.java
+++ src/main/java/com/max/shop/service/BootStrapService.java
@@ -4,11 +4,13 @@ import com.max.shop.generator.ProductGenerator;
 import com.max.shop.repository.ProductRepository;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
 
 @Service
 @Transactional
+@Profile("init-products")
 public class BootStrapService implements ApplicationRunner {
 
     private ProductGenerator productGenerator;
@@ -21,8 +23,8 @@ public class BootStrapService implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) {
-        //        if (productRepository.countProducts() < 10) {
-        //            productGenerator.allProductGeneration();
-        //        }
+        if (productRepository.countProducts() < 10) {
+            productGenerator.allProductGeneration();
+        }
     }
 }
