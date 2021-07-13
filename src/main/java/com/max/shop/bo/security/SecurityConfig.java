766d252d53800a3efcd2926282b40e73acb1267f
maxim_kanapelka <maksim.kanapelka@innowise-group.com>
2021-07-13 11:13:30 +0300

    add jwt for admin

---
+++ src/main/java/com/max/shop/bo/security/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.max.shop.bo.security;
+
+import com.max.shop.bo.security.jwt.JwtConfigurer;
+import com.max.shop.bo.security.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity(debug = true)
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@Slf4j
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    //    TODO: Check it
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/api/admin").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .apply(new JwtConfigurer(jwtTokenProvider));
+
+    }
+}
---
+++ src/main/java/com/max/shop/bo/security/jwt/JwtAuthenticationException.java
@@ -0,0 +1,14 @@
+package com.max.shop.bo.security.jwt;
+
+import org.springframework.security.core.AuthenticationException;
+
+public class JwtAuthenticationException extends AuthenticationException {
+    public JwtAuthenticationException(String msg, Throwable t) {
+        super(msg, t);
+    }
+
+    public JwtAuthenticationException(String msg) {
+        super(msg);
+    }
+}
+
